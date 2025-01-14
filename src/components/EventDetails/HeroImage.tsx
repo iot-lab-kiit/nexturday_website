3c14625d9c81fa77c8119ead5f81a862194152c6
Maanas Sehgal <maanas.sehgal@gmail.com>
2025-01-14 20:15:01 +0530

    Updated home page

--- src/components/EventDetails/HeroImage.tsx
+++ src/components/EventDetails/HeroImage.tsx
@@ -1,17 +1,20 @@
-import React from "react";
+import { useEventStore } from "../../zustand/useEventStore";
 
-const HeroImage: React.FC = () => (
-    <div className="relative w-full lg:w-[600px] h-[400px] flex-shrink-0">
-        <div className="absolute inset-0 rounded-2xl overflow-hidden">
-            <div className="absolute inset-0 bg-gradient-to-br from-purple-500/10 to-blue-500/10 z-10" />
-            <img
-                src="/images/innovance.png"
-                alt="Innovance"
-                className="w-full h-full object-cover object-center bg-zinc-900/50 transition-transform hover:scale-105 duration-700 ease-in-out"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
+const HeroImage = () => {
+    const currentEvent = useEventStore((state) => state.currentEvent);
+    return (
+        <div className="relative w-full lg:w-[600px] h-[400px] flex-shrink-0">
+            <div className="absolute inset-0 rounded-2xl overflow-hidden">
+                <div className="absolute inset-0 bg-gradient-to-br from-purple-500/10 to-blue-500/10 z-10" />
+                <img
+                    src={currentEvent?.images[0].url || "/images/innovance.jpg"}
+                    alt="Innovance"
+                    className="w-full h-full object-cover object-center bg-zinc-900/50 transition-transform hover:scale-105 duration-700 ease-in-out"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
+            </div>
         </div>
-    </div>
-);
+    )
+}
 
-export default HeroImage;
+export default HeroImage
